044e91f2e3b4cf8355dbf6baf6c57ab449d46732
Harsh Mehta <113175089+HarshMehta2522@users.noreply.github.com>
2024-06-20 00:08:47 +0530

    Update node.js

--- fullstack/node/node.js
+++ fullstack/node/node.js
@@ -1,157 +1,94 @@
-//sellandbuyrouter.js
 const e = require("express");
 const express = require("express");
-const SellBuy = require("../mongoose/models/sellBuy");
-const router = express.Router();
-
-router.get("/", async (req, res) => {
-  try {
-    if (req.query.product) {
-      const data = await SellBuy.find({ productName: req.query.product });
-      res.status(200).json(data);
-    } else if (req.query.sortBy) {
-      let list = req.query.sortBy;
-      let ord = -1;
-      if (list.charAt(0) == "l") {
-        ord = 1;
-      }
-      if (list.includes("S")) {
-        const data = await SellBuy.find().sort({ soldPrice: ord });
-        res.status(200).json(data);
-      } else {
-        const data = await SellBuy.find().sort({ costPrice: ord });
-        res.status(200).json(data);
-      }
-    } else {
-      const data = await SellBuy.find();
-      res.status(200).json(data);
-    }
-  } catch (err) {
-    res.status(200).json(data);
-  }
-});
+const SellBuy =require("../mongoose/models/sellBuy")
 
 // setting up the router
-router.post("/", async (req, res, next) => {
-  const data = new SellBuy(req.body);
-  try {
-    if (data.productName.length < 4) {
-      const err = new Error(
-        "product name should have minimum of four characters"
-      );
-      err.status = 400;
-      next(err);
-    } else if (data.costPrice <= 0) {
-      const err = new Error( 
-        "cost price value cannot be zero or negative value"
-      );
-      err.status = 400;
-      next(err);
-    } else if (data.soldPrice <= 0) {
-      const err = new Error(
-        "sold price value cannot be zero or negative value"
-      );
-      err.status = 400;
-      next(err);
-    } else {
-      await data.save();
-      res.status(201).json({ message: "Product Added" });
-    }
-  } catch (err) {
-    res.status(200).json(data);
-  }
-});
-
-router.patch("/:id", async (req, res, next) => {
-  try {
-    const data = req.body.soldPrice;
-    if (data <= 0) {
-      const err = new Error(
-        "sold price value cannot be zero or negative value"
-      );
-      err.status = 400;
-      next(err);
-    } else {
-      res.status(200).json({ message: "Updated Successfully" });
-    }
-  } catch (err) {
-    res.status(200).json(data);
-  }
-});
-
-// router.patch("/:id", async (req, res, next) => {
-//     try {
-//       const { id } = req.params;
-//       const { soldPrice } = req.body;
-  
-//       if (!soldPrice || soldPrice <= 0) {
-//         const err = new Error("Sold price value cannot be zero or negative");
-//         err.status = 400;
-//         throw err;
-//       }
-  
-//       // Find the product by ID and update its sold price
-//       const product = await SellBuy.findByIdAndUpdate(id, { soldPrice }, { new: true });
-  
-//       if (!product) {
-//         const err = new Error("Product not found");
-//         err.status = 404;
-//         throw err;
-//       }
-  
-//       res.status(200).json({ message: "Sold price updated successfully", product });
-//     } catch (err) {
-//       next(err);
-//     }
-//   });
-  
-
-router.delete("/:id", async (req, res) => {
-  try {
-    let id = req.params.id;
-    if (id === undefined || id === "" || id === null) {
-      res.status(400).json(data);
-    }
-    const data = await SellBuy.findByIdAndDelete(id);
-    res.status(200).json({ message: "Deleted successfully" });
-  } catch (err) {
-    res.status(400).json({ error: err });
-  }
-});
 
 const sellAndBuyRouter = new express.Router();
 
 // code goes here for routes
 
+
 // exporting the router
 
-module.exports = router;
+sellAndBuyRouter.delete("/sellProduct/:id",async(req,res)=>{
+    try{
+        let id = req.params.id;
+        if(id=== undefined|| id ==="" || id===null)  
+        res.status(400).json(data)
+        const data= await SellBuy.findByIdAndDelete(id);
+        res.status(200).json({message:"Deleted successfully"});
+    }catch(err){
+        res.status(400).json({error:err});
+    }
+})
 
-//app.js
+sellAndBuyRouter.post("/sellProduct/",async(req,res)=>{
+    const data = new SellBuy(req.body);
+    try{
+        if(data.productName.length<4){
+            res.status(400).json({error:"product name should have minimum of four characters"})
+        }else if(data.costPrice<=0){
+            res.status(400).json({error:"cost price value cannot be zero or negative value"})
 
-const express = require("express");
-const sellAndBuyRouter = require("./routers/sellAndBuy");
-require("./mongoose/connect_db/mongoose");
+        }else {
+            await data.save();
+            res.status(201).json({message:"Product Added"})
+        }
+    }catch(err){
+        res.status(400).json({error:err});
+    }
+})
 
-const app = express();
+sellAndBuyRouter.patch("/sellProduct/:id",async(req,res)=>{
+    try{const id =req.params.id;
+    const body= req.body ;
+    const data= SellBuy.findById(id);
+    if(body.soldPrice<=0)
+        res.status(400).json({error:"sold price value cannot be zero or negative value"})
+    da={
+        soldPrice:body.soldPrice,
+    }
+    await SellBuy.findByIdAndUpdate(id,da);
+    res.status(200).json({message:"Updated Successfully"});}
+    catch(err){
+        res.status(400);
+    }    
+})
 
-app.use(express.json());
-// app.use(bodyParser.json());
+sellAndBuyRouter.get("/sellProduct/",async(req,res)=>{
+    try{
+        if(req.query.product){
+            const data= await SellBuy.find({productName:req.query.product});
+            res.status(200).json(data);
+        }
+        else if(req.query.sortBy){
+            if (req.query.sortBy==="lowerCostPrice"){
+                const data = await SellBuy.find().sort({costPrice:1});
+                res.status(200).json(data);
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "GET,DELETE,PATCH");
-    return res.status(200).json({});
-  }
-  next();
-});
+            }else if (req.query.sortBy==="higherCostPrice"){
+                const data = await SellBuy.find().sort({costPrice:-1});
+                res.status(200).json(data);
 
-app.use("/sellProduct", sellAndBuyRouter);
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({ error: error.message });
-});
+            }else if (req.query.sortBy==="lowerSoldPrice"){
+                const data = await SellBuy.find().sort({soldPrice:1});
+                res.status(200).json(data);
 
-module.exports = app;
+            }else if (req.query.sortBy==="higherSoldPrice"){
+                const data = await SellBuy.find().sort({soldPrice:-1});
+                res.status(200).json(data);
+
+            }
+            
+        }else {
+            const data = await SellBuy.find();
+            res.status(200).json(data);
+
+        }
+    }catch(err){
+        res.status(400);
+    }    
+})
+
+module.exports = sellAndBuyRouter;
